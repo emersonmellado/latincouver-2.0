e82cb1675218595529cf3ef15ce7c58b52a31a9b
Emerson Mellado <emersonmellado@gmail.com>
2017-04-23 20:50:04 -0700

    Adding basic event management handler

--- managementApp/src/js/config.js
+++ managementApp/src/js/config.js
@@ -1,38 +1,31 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  function config($logProvider, toastrConfig, $httpProvider, $authProvider, Api) {
-
-
-
-    $authProvider.httpInterceptor = function() {
-        return true;
-      },
-      $authProvider.withCredentials = false;
-    $authProvider.tokenRoot = null;
-    $authProvider.baseUrl = '/';
-    $authProvider.loginUrl = Api.URL_API + '/authenticate';
-    $authProvider.signupUrl = '/auth/signup';
-    $authProvider.unlinkUrl = '/auth/unlink/';
-    $authProvider.tokenName = 'auth_token';
-    $authProvider.tokenPrefix = 'satellizer';
-    $authProvider.tokenHeader = 'Authorization';
-    $authProvider.tokenType = 'Bearer';
-    $authProvider.storageType = 'localStorage';
-
-    // Http interceptor
-    $httpProvider.interceptors.push('appInterceptor');
-
-    // Enable log
-    $logProvider.debugEnabled(true);
-
-    // Set options third-party lib
-    toastrConfig.allowHtml = true;
-    toastrConfig.timeOut = 3000;
-    toastrConfig.positionClass = 'toast-top-right';
-    toastrConfig.preventDuplicates = false;
-    toastrConfig.progressBar = true;
-  }
-
-  angular.module('lcv2').config(['$logProvider', 'toastrConfig', '$httpProvider', '$authProvider', 'Api', config]);
+    function config($logProvider, toastrConfig, $httpProvider, $authProvider, Api) {
+        $authProvider.httpInterceptor = function() {
+            return true;
+        };
+        $authProvider.withCredentials = false;
+        $authProvider.tokenRoot = null;
+        $authProvider.baseUrl = '/';
+        $authProvider.loginUrl = Api.URL_API + '/authenticate';
+        $authProvider.signupUrl = '/auth/signup';
+        $authProvider.unlinkUrl = '/auth/unlink/';
+        $authProvider.tokenName = 'auth_token';
+        $authProvider.tokenPrefix = 'satellizer';
+        $authProvider.tokenHeader = 'Authorization';
+        $authProvider.tokenType = 'Bearer';
+        $authProvider.storageType = 'localStorage';
+        // Http interceptor
+        $httpProvider.interceptors.push('appInterceptor');
+        // Enable log
+        $logProvider.debugEnabled(true);
+        // Set options third-party lib
+        toastrConfig.allowHtml = true;
+        toastrConfig.timeOut = 3000;
+        toastrConfig.positionClass = 'toast-top-right';
+        toastrConfig.preventDuplicates = false;
+        toastrConfig.progressBar = true;
+    }
+    angular.module('lcv2').config(['$logProvider', 'toastrConfig', '$httpProvider', '$authProvider', 'Api', config]);
 })();
--- managementApp/src/js/constants.js
+++ managementApp/src/js/constants.js
@@ -1,11 +1,7 @@
 (function() {
-  'use strict';
-
-  angular
-    .module('lcv2')
-    .constant('Api', {
-      //URL_API: 'http://localhost:4000/v1'       
-      URL_API: 'https://thinkiapi.herokuapp.com/v1'
+    'use strict';
+    angular.module('lcv2').constant('Api', {
+        URL_API: 'http://localhost:3000/'
+            //URL_API: 'https://thinkiapi.herokuapp.com/v1'
     });
-
 })();
