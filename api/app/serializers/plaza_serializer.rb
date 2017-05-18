97db851d3e078f664cd167c02093079780347954
Emerson <emersonmellado@gmail.com>
2017-05-18 00:38:40 -0700

    Adjusting Seed and plaza serialization

--- api/app/serializers/plaza_serializer.rb
+++ api/app/serializers/plaza_serializer.rb
@@ -1,5 +1,6 @@
 class PlazaSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image_url, :longitude, :latitude, :active
+  attributes :id, :event_id, :css_style_id, :name, :description, :image_url, :longitude, :latitude, :active
   #has_one :user
   has_one :css_style
+  has_one :event
 end
