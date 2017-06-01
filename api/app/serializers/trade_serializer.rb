460e1812b40e10a2e746b870f63f010021f1a12d
Emerson Mellado <emersonmellado@gmail.com>
2017-05-31 23:37:42 -0700

    Adjusting trade serializer

--- api/app/serializers/trade_serializer.rb
+++ api/app/serializers/trade_serializer.rb
@@ -2,6 +2,6 @@ class TradeSerializer < BaseSerializer
   attributes :id, :name, :description, :short_description, :order, :image_url, :active
   has_one :trade_type
   has_one :trade_group
-  has_many :trade_product
-  has_many :trade_link
+  has_many :trade_products
+  has_many :trade_links
 end
